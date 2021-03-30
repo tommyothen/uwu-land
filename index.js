f68346f9195eec82c24d4c3591f4c96c99af5cec
DaSushiAsian <42751764+DaSushiAsian@users.noreply.github.com>
2021-03-30 22:33:37 +0100

    Use env variables no matter if it's in production or not

--- index.js
+++ index.js
@@ -10,28 +10,24 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const followRedirects = require("follow-redirects");
 
-if (process.env.NODE_ENV == "production") {
-  admin.initializeApp({
-    credential: admin.credential.applicationDefault()
-  });
-} else {
-  const credentials = {
-    type: process.env.type,
-    project_id: process.env.project_id,
-    private_key_id: process.env.private_key_id,
-    private_key: process.env.private_key,
-    client_email: process.env.client_email,
-    client_id: process.env.client_id,
-    auth_uri: process.env.auth_uri,
-    token_uri: process.env.token_uri,
-    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
-    client_x509_cert_url: process.env.client_x509_cert_url
-  };
+const credentials = {
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key,
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url
+};
 
-  admin.initializeApp({
-    credential: admin.credential.cert(credentials)
-  });
-}
+console.log(credentials);
+
+admin.initializeApp({
+  credential: admin.credential.cert(credentials)
+});
 const db = admin.firestore();
 
 const app = express();
